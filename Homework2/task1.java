189a030f745ab91c324e1ea5d573aa754a56c1f2
TotskayaOV <olga.totskaya@gmail.com>
2023-02-15 17:02:20 +0700

    Created seminar02

---
+++ Homework2/task1.java
@@ -0,0 +1,12 @@
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть  WHERE этого запроса, используя StringBuilder. 
+// Данные для фильтрации приведены ниже в виде json строки. Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+package Homework2;
+
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
