e1a7e6c3112a552802dba1fd16f4a99b4807e2ec
TotskayaOV <olga.totskaya@gmail.com>
2023-02-18 21:50:12 +0700

    Finish Homewor 2, Homework 3

--- Homework1/task3.java
+++ Homework1/task3.java
@@ -1,6 +1,8 @@
-// Реылизовать простой калькулятор
+// Реализовать простой калькулятор
 package Homework1;
 import java.util.Scanner;
+import java.util.logging.*;
+import java.io.IOException;
 
 public class task3 {
     public static void main(String[] args) {
@@ -13,25 +15,53 @@ public class task3 {
         Scanner iScanner3 = new Scanner(System.in);
         System.out.printf("Введите оператор(+, -, *, /): ");
         String z = iScanner3.nextLine();
+        float result = 0;
         switch (z) {
             case "+":
                 int res_sum = x + y;
+                result = (float)res_sum;
+                // lg_result(x, y, res_sum, z);
                 System.out.printf("%d + %d = %d. \n", x, y, res_sum);
                 break;
             case "-":
                 int res = x - y;
+                result = (float)res;
                 System.out.printf("%d - %d = %d. \n", x, y, res);
                 break;
             case "*":
                 int res_mult = x * y;
+                result = (float)res_mult;
                 System.out.printf("%d * %d = %d. \n", x, y, res_mult);
                 break;
             case "/":
-                Float res_div = (float)x / (float)y;
+                float res_div = (float)x / (float)y;
+                result = res_div;
                 System.out.printf("%d : %d = %f. \n", x, y, res_div);
                 break;
             default:
                 break;
+            }
+        try {
+            String log_string = Integer.toString(x) + z + Integer.toString(y) + "=" + Float.toString(result);
+            lg_result(log_string);
+        } catch (Exception e) {
+            System.err.println("Неправильный формат строки!");
         }
+        
+    }
+    static void lg_result(String oper) throws IOException {
+        Logger log = Logger.getLogger(task4.class.getName());
+        FileHandler fh =new FileHandler("Homework1/logCalc.txt", true);
+        log.addHandler(fh);
+        log.setUseParentHandlers(false);
+
+        SimpleFormatter sf = new SimpleFormatter();
+        fh.setFormatter(sf);
+        System.out.println(oper);
+        StringBuilder sb = new StringBuilder("Результат вычисления: ");
+        sb.append(oper);
+        String res = sb.toString();
+
+        log.log(Level.INFO, res);
     }  
 }
--- Homework2/task3.java
+++ Homework2/task3.java
@@ -7,7 +7,52 @@
 // Студент Краснов получил 5 по предмету Физика.
 
 package Homework2;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class task3 {
-    
+    public static void main(String[] args) throws IOException {
+
+        String jsonStr = Files.readString(Paths.get("data.json"));
+        String result = getString(jsonStr); // парсинг и формирование заданной строки
+        System.out.println(result);
+    }
+
+    private static String getString(String jsonStr) {
+        Gson gson = new Gson();
+        Student[] students = gson.fromJson(jsonStr, Student[].class);
+        StringBuilder sb = new StringBuilder();
+        for (Student student : students) {
+            sb.append("Студент ");
+            sb.append(student.surname);
+            sb.append(" получил ");
+            sb.append(student.mark);
+            sb.append(" по предмету ");
+            sb.append(student.subject);
+            sb.append("\n");
+
+        }
+        return sb.toString();
+    }
+
+    private static class Student {
+        @SerializedName("фамилия")
+        String surname;
+        @SerializedName("оценка")
+        String mark;
+        @SerializedName("предмет")
+        String subject;
+
+        public Student(String surname, String mark, String subject) {
+            this.surname = surname;
+            this.mark = mark;
+            this.subject = subject;
+
+        }
+    }
 }
