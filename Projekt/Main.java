67aa75b92257235304710778681e2b11783e33c7
TotskayaOV <olga.totskaya@gmail.com>
2023-03-02 13:15:54 +0700

    2 commit

--- Projekt/Main.java
+++ Projekt/Main.java
@@ -7,6 +7,41 @@ import java.util.Set;
 
 
 public class Main {
+    public OurNotebook userVois(OurNotebook notebookUser) {
+            Scanner iScanner = new Scanner(System.in);
+            System.out.printf("Какие параметры вы хотите видеть у своего ноутбука:\n1. Оперативная память\n2. Объем жесткого диска\n3. Предустановленная ОС\n4. цвет\nВведите номер пункта меню для выбора или введите 0");
+            int userChoise = iScanner.nextInt();
+            switch (userChoise) {
+                case 1:
+                    Scanner iScanner = new Scanner(System.in);
+                    System.out.printf("Введите желаемое количество Оперативной памяти (число): ");
+                    int userRum = iScanner.nextInt();
+                    notebookUser.ram = userRum;
+                    break;
+                case 2:
+                    Scanner iScanner = new Scanner(System.in);
+                    System.out.printf("Введите желаемый объем жесткого диска (число): ");
+                    int userHdd = iScanner.nextInt();
+                    notebookUser.hdd = userHdd;
+                    break;
+                case 3:
+                    Scanner iScanner = new Scanner(System.in);
+                    System.out.printf("Введите желаемую операционную систему: ");
+                    String userOSystem = iScanner.nextLine();
+                    notebookUser.oSystem = userOSystem;
+                    break;
+                case 4:
+                    Scanner iScanner = new Scanner(System.in);
+                    System.out.printf("Введите желаемый цвет ноутбука: ");
+                    String userColore = iScanner.nextLine();
+                    notebookUser.colore = userColore;
+                    break;
+                default:
+                    break;
+            }
+            iScanner.close();
+            return notebookUser;
+        }
 
     public static void main(String[] args) {
         Set<OurNotebook> set = new HashSet<>();
@@ -31,68 +66,9 @@ public class Main {
 
         // System.out.println(notebook5.equals(notebook6));
         // System.out.println(notebook5.equality(notebook6));
+        
+        
         OurNotebook notebookUserchoise = new OurNotebook("None", "None", "None", 0, 0, "None", "None");
-
-        public void userVois(Object notebookUser) {
-            Scanner iScanner = new Scanner(System.in);
-            System.out.printf("Какие параметры вы хотите видеть у своего ноутбука:\n1. Оперативная память\n2. Объем жесткого диска\n"); 
-            System.out.printf("3. Предустановленная ОС\n4. цвет\nВведите номер пункта меню для выбора или введите 0");
-            int userChoise = iScanner.nextInt();
-            switch (userChoise) {
-                case 1:
-                    Scanner iScanner = new Scanner(System.in);
-                    System.out.printf("Введите желаемое количество Оперативной памяти (число): ");
-                    int userRum = iScanner.nextInt();
-                    notebookUserchoise.ram = userRum;
-                    break;
-                case 2:
-                    Scanner iScanner = new Scanner(System.in);
-                    System.out.printf("Введите желаемый объем жесткого диска (число): ");
-                    int userHdd = iScanner.nextInt();
-                    notebookUserchoise.hdd = userHdd;
-                    break;
-                case 3:
-                    Scanner iScanner = new Scanner(System.in);
-                    System.out.printf("Введите желаемую операционную систему: ");
-                    String userOSystem = iScanner.nextInt();
-                    notebookUserchoise.oSystem = userOSystem;
-                    break;
-                case 4:
-                    Scanner iScanner = new Scanner(System.in);
-                    System.out.printf("Введите желаемый цвет ноутбука: ");
-                    String userColore = iScanner.nextInt();
-                    notebookUserchoise.colore = userColore;
-                    break;
-                default:
-                    break;
-            }
-            iScanner.close();
-        }
-    userVois(notebookUserchoise);
+        notebookUserchoise = userVois(notebookUserchoise);
     }
 }
-    
-            // System.out.println(userRum);
-        // System.out.println(userHdd);
-
-        // System.out.println("Все ноутбуки c такими параметрами в наличии:");
-        // Iterator<OurNotebook> iterator = set.iterator();
-        // while (iterator.hasNext()) {
-        //     OurNotebook element = iterator.next();
-        //     if (element.ram >= userRum){
-        //         if (element.hdd >= userHdd){
-        //             System.out.println(element);
-        //         }
-        //     }        
-        // }
-
-        // Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-// Создать множество ноутбуков.
-// Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки, отвечающие фильтру. 
-// Критерии фильтрации можно хранить в Map. Например: “Введите цифру, соответствующую необходимому критерию:
-// 1 - ОЗУ
-// 2 - Объем ЖД
-// 3 - Операционная система
-// 4 - Цвет …
-// Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
-// Отфильтровать ноутбуки из первоначального множества и вывести проходящие по условиям.
